ce3fc6f529132b46d140f2dd35beb69c3ce4eb48
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-08-09 04:21:54 -0500

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  # Static Pages
+  get '/about', to: 'pages#about', as: :about_page
+  get '/home', to: 'pages#home', as: :home_page
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
 end
