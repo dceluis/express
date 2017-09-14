49972e2807d007b25235a0295e96756f4a343ce2
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-14 18:16:28 -0500

    User Factory

--- spec/views/users/index.html.slim_spec.rb
+++ spec/views/users/index.html.slim_spec.rb
@@ -1,25 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+
   before(:each) do
-    assign(:users, [
-      User.create!(
-        first_name: "First Name",
-        last_name: "Last Name",
-        email: "Email"
-      ),
-      User.create!(
-        first_name: "First Name",
-        last_name: "Last Name",
-        email: "Email"
-      )
-    ])
+    assign(:users, [ user1, user2 ])
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", text: "First Name".to_s, count: 2
-    assert_select "tr>td", text: "Last Name".to_s, count: 2
-    assert_select "tr>td", text: "Email".to_s, count: 2
+    assert_select "tr>td", text: user1.first_name.to_s, count: 1
+    assert_select "tr>td", text: user1.last_name.to_s, count: 1
+    assert_select "tr>td", text: user1.email.to_s, count: 1
+    assert_select "tr>td", text: user2.first_name.to_s, count: 1
+    assert_select "tr>td", text: user2.last_name.to_s, count: 1
+    assert_select "tr>td", text: user2.email.to_s, count: 1
   end
 end
--- spec/views/users/show.html.slim_spec.rb
+++ spec/views/users/show.html.slim_spec.rb
@@ -2,17 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "users/show", type: :view do
   before(:each) do
-    @user = assign(:user, User.create!(
-      first_name: "First Name",
-      last_name: "Last Name",
-      email: "Email"
-    ))
+    @user = assign(:user, create(:user))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/First Name/)
-    expect(rendered).to match(/Last Name/)
-    expect(rendered).to match(/Email/)
+    expect(rendered).to match(%r/#{@user.first_name}/)
+    expect(rendered).to match(%r/#{@user.last_name}/)
+    expect(rendered).to match(%r/#{@user.email}/)
   end
 end
