4cfb9fc5c49ece44ceb66ae9d080009f22d111b9
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-14 18:18:30 -0500

    Completely remove business as user descendant

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20170914093531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "businesses", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
