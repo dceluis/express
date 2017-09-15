7cf63d9a665c9dcb393eacc03c289d85c91694ee
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-15 15:11:40 -0500

    Update Factory for Sorcery

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,16 +5,11 @@ FactoryGirl.define do
     email { Faker::Internet.safe_email }
     password { Faker::Internet.password }
 
-    trait :confirmed do
-      confirmed_at Time.current
-    end
-
     factory :admin do
       first_name { 'Luis Felipe' }
       last_name { 'Sanchez' }
       email { 'luis@express.com' }
       password { 'zUgY66yi' }
-      confirmed_at Time.current
     end
   end
 end
