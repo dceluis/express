81db65b4acbe426a6fcf86ab0fa18f4601584de0
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-15 15:12:09 -0500

    User Sorcery migrations

--- db/migrate/20170915082334_sorcery_core.rb
+++ db/migrate/20170915082334_sorcery_core.rb
@@ -1,6 +1,8 @@
 class SorceryCore < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
       t.string :email,            :null => false
       t.string :crypted_password
       t.string :salt
