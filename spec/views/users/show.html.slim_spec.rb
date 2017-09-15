3138fd03f8c67b062066abf76e2b0f28f9eeb58f
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-14 19:12:34 -0500

    Escape html in match tests

--- spec/views/users/show.html.slim_spec.rb
+++ spec/views/users/show.html.slim_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe "users/show", type: :view do
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(%r/#{@user.first_name}/)
-    expect(rendered).to match(%r/#{@user.last_name}/)
-    expect(rendered).to match(%r/#{@user.email}/)
+    expect(rendered).to match(%r/#{CGI.escape_html(@user.first_name)}/)
+    expect(rendered).to match(%r/#{CGI.escape_html(@user.last_name)}/)
+    expect(rendered).to match(%r/#{CGI.escape_html(@user.email)}/)
   end
 end
