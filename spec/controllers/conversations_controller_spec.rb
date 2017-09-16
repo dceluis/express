5cc87022c156515536aef63e90eeaf25b56fd696
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-16 03:56:50 -0500

    Add some rubocop corrections

--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe ConversationsController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       conversation = Conversation.create! valid_attributes
-      get :show, params: {id: conversation.to_param}, session: valid_session
+      get :show, params: { id: conversation.to_param }, session: valid_session
       expect(response).to be_success
     end
   end
@@ -67,7 +67,7 @@ RSpec.describe ConversationsController, type: :controller do
   describe "GET #edit" do
     it "returns a success response" do
       conversation = Conversation.create! valid_attributes
-      get :edit, params: {id: conversation.to_param}, session: valid_session
+      get :edit, params: { id: conversation.to_param }, session: valid_session
       expect(response).to be_success
     end
   end
@@ -76,19 +76,19 @@ RSpec.describe ConversationsController, type: :controller do
     context "with valid params" do
       it "creates a new Conversation" do
         expect {
-          post :create, params: {conversation: valid_attributes}, session: valid_session
+          post :create, params: { conversation: valid_attributes }, session: valid_session
         }.to change(Conversation, :count).by(1)
       end
 
       it "redirects to the created conversation" do
-        post :create, params: {conversation: valid_attributes}, session: valid_session
+        post :create, params: { conversation: valid_attributes }, session: valid_session
         expect(response).to redirect_to(Conversation.last)
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {conversation: invalid_attributes}, session: valid_session
+        post :create, params: { conversation: invalid_attributes }, session: valid_session
         expect(response).to be_success
       end
     end
@@ -102,14 +102,14 @@ RSpec.describe ConversationsController, type: :controller do
 
       it "updates the requested conversation" do
         conversation = Conversation.create! valid_attributes
-        put :update, params: {id: conversation.to_param, conversation: new_attributes}, session: valid_session
+        put :update, params: { id: conversation.to_param, conversation: new_attributes }, session: valid_session
         conversation.reload
         skip("Add assertions for updated state")
       end
 
       it "redirects to the conversation" do
         conversation = Conversation.create! valid_attributes
-        put :update, params: {id: conversation.to_param, conversation: valid_attributes}, session: valid_session
+        put :update, params: { id: conversation.to_param, conversation: valid_attributes }, session: valid_session
         expect(response).to redirect_to(conversation)
       end
     end
@@ -117,7 +117,7 @@ RSpec.describe ConversationsController, type: :controller do
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'edit' template)" do
         conversation = Conversation.create! valid_attributes
-        put :update, params: {id: conversation.to_param, conversation: invalid_attributes}, session: valid_session
+        put :update, params: { id: conversation.to_param, conversation: invalid_attributes }, session: valid_session
         expect(response).to be_success
       end
     end
@@ -127,13 +127,13 @@ RSpec.describe ConversationsController, type: :controller do
     it "destroys the requested conversation" do
       conversation = Conversation.create! valid_attributes
       expect {
-        delete :destroy, params: {id: conversation.to_param}, session: valid_session
+        delete :destroy, params: { id: conversation.to_param }, session: valid_session
       }.to change(Conversation, :count).by(-1)
     end
 
     it "redirects to the conversations list" do
       conversation = Conversation.create! valid_attributes
-      delete :destroy, params: {id: conversation.to_param}, session: valid_session
+      delete :destroy, params: { id: conversation.to_param }, session: valid_session
       expect(response).to redirect_to(conversations_url)
     end
   end
