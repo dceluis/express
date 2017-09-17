09a8bcd4ade84db8c17697dcd8524528d5f20ac1
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-16 20:21:03 -0500

    Add duplicate conversation validation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,8 +4,20 @@ class Conversation < ApplicationRecord
   has_many :messages
 
   validates_uniqueness_of :sender_id, scope: :recipient_id
+  validate :is_not_a_duplicate?
 
   def title
     "#{sender.first_name} - #{recipient.first_name}"
+  end
+
+  private
+
+  def is_not_a_duplicate?
+    if Conversation.find_by(sender_id: recipient_id, recipient_id: sender_id)
+      errors.add(:id,:already_exist, message: 'A conversation already exists between these users')
+      false
+    else
+      true
+    end
   end
 end
