691b6343ee215a6d8e1d7e2548b63e47a70486cc
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-21 02:14:50 -0500

    Mount actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   get '/home', to: 'pages#home', as: :home_page
 
   resources :users
-  resources :conversations, shallow: true do
+  resources :conversations, shallow: true, except: %i(create) do
     resources :messages
   end
 
+  post 'conversations/:user_id', to: 'conversations#find_or_create'
+
   resource :session, only: %i{new create destroy}
+
+  mount ActionCable.server => '/cable'
 end
