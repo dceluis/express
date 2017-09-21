e6a87ecfed3ad4655b83aa58e2c9fa992500f71b
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-21 02:16:46 -0500

    Update views and change the way ne conversations are created

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,5 @@
 class ConversationsController < ApplicationController
-  before_action :set_conversation, only: [:show, :edit, :update, :destroy]
+  before_action :set_conversation, only: [:show, :edit, :update]
 
   # GET /conversations
   # GET /conversations.json
@@ -12,63 +12,41 @@ class ConversationsController < ApplicationController
   def show
   end
 
-  # GET /conversations/new
-  def new
-    @conversation = Conversation.new
-  end
-
   # GET /conversations/1/edit
   def edit
   end
 
-  # POST /conversations
-  # POST /conversations.json
-  def create
-    @conversation = Conversation.new(conversation_params)
-
-    respond_to do |format|
-      if current_user.sent_conversations << @conversation
-        format.html { redirect_to @conversation, notice: 'Conversation was successfully created.' }
-        format.json { render :show, status: :created, location: @conversation }
-      else
-        format.html { render :new }
-        format.json { render json: @conversation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PATCH/PUT /conversations/1
   # PATCH/PUT /conversations/1.json
-  def update
-    respond_to do |format|
-      if @conversation.update(conversation_params)
-        format.html { redirect_to @conversation, notice: 'Conversation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @conversation }
-      else
-        format.html { render :edit }
-        format.json { render json: @conversation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @conversation.update(conversation_params)
+  #       format.html { redirect_to @conversation, notice: 'Conversation was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @conversation }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @conversation.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /conversations/1
-  # DELETE /conversations/1.json
-  def destroy
-    @conversation.destroy
+  # POST /conversations/1
+  # POST /conversations/1.json
+  def find_or_create
+    other_user = User.find(params[:user_id])
+    @conversation = Conversation.exactly_between( current_user, other_user ).first || Conversation.create( users: [ current_user, other_user ] )
+
     respond_to do |format|
-      format.html { redirect_to conversations_url, notice: 'Conversation was successfully destroyed.' }
-      format.json { head :no_content }
+      if @conversation
+        format.json { redirect_to conversation_messages_url(@conversation) }
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_conversation
-      @conversation = Conversation.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def conversation_params
-      params.require(:conversation).permit(:recipient_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_conversation
+    @conversation = Conversation.find(params[:id])
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,73 +1,14 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
-  before_action :set_conversation, only: [:index, :new, :create]
+  before_action :set_conversation, only: [:index]
 
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.where(conversation: @conversation)
-  end
-
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-  end
-
-  # GET /messages/new
-  def new
-    @message = Message.new
-  end
-
-  # GET /messages/1/edit
-  def edit
-  end
-
-  # POST /messages
-  # POST /messages.json
-  def create
-    @message = Message.new(message_params)
-    @message.user = current_user
-
-    respond_to do |format|
-      if @conversation.messages << @message
-        format.html { redirect_to conversation_messages_url(@message.conversation), notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to conversation_messages_url(@message.conversation), notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /messages/1
-  # DELETE /messages/1.json
-  def destroy
-    @message.destroy
-    respond_to do |format|
-      format.html { redirect_to conversation_messages_url(@message.conversation), notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @messages = @conversation.messages
+    @message  = Message.new
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_message
-    @message = Message.find(params[:id])
-  end
 
   def set_conversation
     @conversation = Conversation.find(params[:conversation_id])
