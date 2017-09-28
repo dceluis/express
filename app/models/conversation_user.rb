e2d519c2c2bc880b986b39397f18f9da3d386be8
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-28 16:35:33 -0500

    SEt dependent: :destroy for ConversationUser messages

--- app/models/conversation_user.rb
+++ app/models/conversation_user.rb
@@ -2,7 +2,7 @@ class ConversationUser < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :user, presence: true, uniqueness: { scope: :conversation }
   validates :conversation, presence: true
