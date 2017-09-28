d18ff65b1a12d60e0ab6aafe2da088aec58ba3f4
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-28 16:35:00 -0500

    Create not_authenticated and user_not_authorized on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :error
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+  private
+
   def not_authenticated
     redirect_to new_session_url, error: 'You need to login to see that page.'
+  end
+
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
