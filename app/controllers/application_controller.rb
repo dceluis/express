0a48e32f38aca8021b81a9cbfd313208ed95c2ac
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-28 14:31:53 -0500

    Create ApplicationController#not_authenticated method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  # after_action :verify_authorized
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :error
+
+  def not_authenticated
+    redirect_to new_session_url, error: 'You need to login to see that page.'
+  end
 end
