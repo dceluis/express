2d95dab2d3f312b8057079cac2dfead139b78366
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-29 18:23:51 -0500

    Change GET messages request spec to test for user redirect on user no authenticated

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "Messages", type: :request do
   describe "GET /messages" do
     let(:conversation) { create(:conversation) }
-    it "works! (now write some real specs)" do
+    it "redirects if user not logged in" do
       get conversation_messages_path(conversation)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
