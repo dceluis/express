8c130c5f207fc24d198a7f82d5ab531672d60f84
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-29 18:25:51 -0500

    Rework messages/index view spec * It wasn't referencing users correctly because conversation was only stubbed. * Assigned @other as instance variable * Made 2 different messages and checked for correct display of each corresponding author

--- spec/views/messages/index.html.slim_spec.rb
+++ spec/views/messages/index.html.slim_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "messages/index", type: :view do
-  let(:conversation) { build_stubbed(:conversation) }
-  let(:conversation_user) { conversation.conversation_users.first }
-  let!(:message1) { build_stubbed(:message, conversation_user: conversation_user, content: 'Hello world') }
-  let!(:message2) { build_stubbed(:message, conversation_user: conversation_user, content: 'Hello world') }
+  let(:conversation) { create(:conversation) }
+  let(:conversation_user1) { conversation.conversation_users.first }
+  let(:conversation_user2) { conversation.conversation_users.last }
+  let!(:message1) { create(:message, conversation_user: conversation_user1, content: 'Hello world') }
+  let!(:message2) { create(:message, conversation_user: conversation_user2, content: 'Hello Back') }
+  let!(:new_message) { build(:message, conversation_user: conversation_user1) }
 
   before(:each) do
     assign(:conversation, conversation)
+    assign(:other, conversation_user2.user )
     assign(:messages, [ message1, message2 ])
+    assign(:message, new_message)
   end
 
   it "renders a list of messages" do
