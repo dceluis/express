184739787875ad8ade4025ef8826e0770f1f9026
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-29 12:31:31 -0500

    Set carrierwave initiliazer to porduction only.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,14 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => 'sa-east-1'
-  }
-  config.fog_directory  = 'express-link'
-  config.fog_public     = false
-  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => 'sa-east-1'
+    }
+    config.fog_directory  = 'express-link'
+    config.fog_public     = false
+    config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+  end
 end
