9ad945b39f8432048064e8214b32d8cba0d8c701
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-09-29 13:58:00 -0500

    Add host to CarrierWave config initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,12 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'sa-east-1'
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => ENV['S3_REGION'],
+      :host                   => ENV['S3_ASSET_URL']
     }
-    config.fog_directory  = 'express-link'
+    config.fog_directory  = ENV['S3_BUCKET']
     config.fog_public     = false
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
   end
