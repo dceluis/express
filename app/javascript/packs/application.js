6484aac025cd58bb1a319baebf8d6ca73eeb8b2f
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-10-02 04:23:13 -0500

    Serve all stylesheets with webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import '../scss/application.scss'
+
 console.log('Hello World from Webpacker')
 
 import { InteriorLeftNav } from 'carbon-components'
