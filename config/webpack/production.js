fba4d2681cc844845abe69d1112fd10ff2bbbf46
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-10-03 20:42:51 -0500

    Disable uglifyjs plugin on production build

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,7 @@
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 const environment = require('./environment')
+
+environment.plugins.delete('UglifyJs')
+environment.plugins.set( 'Minify', new MinifyPlugin )
 
 module.exports = environment.toWebpackConfig()
