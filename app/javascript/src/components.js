21f7923f543cc0157b0be3f4f1e9fe9585193c61
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-12-19 18:49:52 -0500

    Remove carboncomponents dependency

--- app/javascript/src/components.js
+++ app/javascript/src/components.js
@@ -1,15 +1,1 @@
 import './components/message-tag.js'
-
-// import { InteriorLeftNav } from 'carbon-components/umd'
-// import { DataTable } from 'carbon-components/umd'
-// import { DetailPageHeader } from 'carbon-components/umd'
-// import { FileUploader } from 'carbon-components/umd'
-// import { OverflowMenu } from 'carbon-components/umd'
-
-// document.addEventListener('turbolinks:load', () => {
-//   InteriorLeftNav.init();
-//   DataTable.init();
-//   DetailPageHeader.init();
-//   FileUploader.init();
-//   OverflowMenu.init();
-// });
