2499747ac7a6517ebf3a54e846dd1aea44ade468
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-12-20 10:45:35 -0500

    Rename components.js to messages.js

--- app/javascript/src/messages.js
+++ app/javascript/src/messages.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Punchbox from 'punchbox-js'
 import App from './components/app.vue'
+import VueResource from 'vue-resource'
+import TurbolinksAdapter from 'vue-turbolinks'
+
+Vue.use(VueResource)
+Vue.use(TurbolinksAdapter)
 
 Punchbox.on('Messages', {
   index: function (){
