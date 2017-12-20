96ecae286923f4e37919e782b15802f1d10e5a24
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-12-19 23:03:02 -0500

    Implement punchbox and include test app.vue

--- app/javascript/src/components.js
+++ app/javascript/src/components.js
@@ -1,1 +1,15 @@
-import './components/message-tag.js'
+import Vue from 'vue'
+import Punchbox from 'punchbox-js'
+import App from './components/app.vue'
+
+Punchbox.on('Messages', {
+  index: function (){
+
+    document.body.appendChild(document.createElement('hello'))
+    const app = new Vue({
+      render: h => h(App)
+    }).$mount('hello')
+
+    console.log(app)
+  }
+})
