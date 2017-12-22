ddf2dc754aaf2bb7b8f6b9414821efa4016da319
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-12-22 18:25:30 -0500

    Update travis.yml to test for webpack build.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,6 @@ const MinifyPlugin = require("babel-minify-webpack-plugin");
 const environment = require('./environment')
 
 environment.plugins.delete('UglifyJs')
-environment.plugins.set( 'Minify', new MinifyPlugin )
+environment.plugins.append( 'Minify', new MinifyPlugin({}, { sourceMap: null }) )
 
 module.exports = environment.toWebpackConfig()
